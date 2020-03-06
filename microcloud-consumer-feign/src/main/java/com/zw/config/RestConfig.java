04155927e24c1014b0234af8f56e39ed137e8c35
zwarthas <375052868@qq.com>
2020-03-06 19:50:37 +0800

    z

---
+++ microcloud-consumer-feign/src/main/java/com/zw/config/RestConfig.java
@@ -0,0 +1,39 @@
+package com.zw.config;
+
+import java.nio.charset.Charset;
+import java.util.Base64;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.client.RestTemplate;
+
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+
+@Configuration
+public class RestConfig {
+
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
+	@Bean
+	public HttpHeaders httpHeaders() {
+		HttpHeaders headers = new HttpHeaders(); // 定义一个HTTP的头信息
+		String auth = "admin:123456"; // 认证的原始信息
+		byte[] encodedAuth = Base64.getEncoder().encode(auth.getBytes(Charset.forName("US-ASCII"))); // 进行一个加密的处理"US-ASCII"
+		String authHeader = "Basic " + new String(encodedAuth);
+		headers.set("Authorization", authHeader);
+		return headers;
+	}
+	
+	@Bean
+	public IRule ribbonRule() {
+		return new RandomRule();
+	}
+
+}
---
+++ microcloud-consumer-feign/src/main/java/com/zw/controller/ConsumerProductController.java
@@ -0,0 +1,65 @@
+package com.zw.controller;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+import javax.websocket.server.PathParam;
+
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.zw.constants.ProductConstants;
+import com.zw.vo.Product;
+
+@RestController
+@RequestMapping("/consumer")
+public class ConsumerProductController {
+
+	@Resource
+	private HttpHeaders httpHeaders;
+
+	@Resource
+	private RestTemplate restTemplate;
+	
+	@Resource
+	LoadBalancerClient loadBalancerClient;
+
+	@RequestMapping("/product/get/{id}")
+	public Object getProduct(@PathVariable("id") long id) {
+		Product p= restTemplate.exchange(ProductConstants.PRODUCT_GET_URL+id, HttpMethod.GET,
+				new HttpEntity<>(httpHeaders), Product.class).getBody();
+		return p;
+	}
+
+	@RequestMapping("/product/list")
+	public Object listProduct() {
+		ServiceInstance serviceInstance=loadBalancerClient.choose("MICROCLOUD-PROVIDER-PRODUCT");
+		System.out.println(
+                "【*** ServiceInstance ***】host = " + serviceInstance.getHost()
+                        + "、port = " + serviceInstance.getPort()
+                        + "、serviceId = " + serviceInstance.getServiceId());
+		List<Product> list = restTemplate
+				.exchange(ProductConstants.PRODUCT_LIST_URL, HttpMethod.GET, new HttpEntity<>(httpHeaders), List.class)
+				.getBody();
+		return list;
+	}
+	
+	@RequestMapping("/product/add")
+	public Object addProduct(@RequestBody Product product) {
+		Boolean result=restTemplate.exchange(ProductConstants.PRODUCT_ADD_URL, HttpMethod.POST,
+				new HttpEntity<>(product,httpHeaders), Boolean.class).getBody();
+		return result;
+	}
+	
+	
+
+}
